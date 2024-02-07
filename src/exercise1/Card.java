c2e2999c3940b45933b8b12c26292d2c3aa3bb8a
sukhd <sukhd@10.48.26.188>
2024-02-07 11:55:08 -0500

    edited code

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -14,14 +14,25 @@ public class Card {
 
    private String suit; //clubs, spades, diamonds, hearts
    private int value;//1-13
+   
+       public Card(String suit, int value) {
+        this.suit = suit;
+        this.value = value;
+    }
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+
+    Card() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     /**
      * @return the suit
      */
     public String getSuit() {
         return suit;
     }
+
+
 
     /**
      * @param suit the suit to set
